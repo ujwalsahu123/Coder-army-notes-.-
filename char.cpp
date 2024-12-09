7f8155c7112a74441eabfc52d45f5eab6e59f981
ujwalsahu123 <132041436+ujwalsahu123@users.noreply.github.com>
2024-12-09 08:22:58 +0000

    connected

---
+++ char.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    char ch = 'a' ;
+    int var = 'a'+ 2;
+    char ch2 = 67 ;
+    int var2 = 'a' ;
+    cout<< "hello"<<ch<<endl<<var<<endl<<ch2<<endl<<var2<<endl ;
+
+    char ch = 'a' ;
+    
+    if(ch == 'a')
+    cout<<"yes"<<endl;
+
+    if(ch==97)
+    cout<<"yes"<<endl;
+
+    int x = 'a';
+
+    if(ch==x)
+    cout<<"yes"<<endl;
+    
+    
+    
+}
