f2ebe7dfc0fa1110e3d9f39502232c7a12f96e5f
ujwalsahu123 <132041436+ujwalsahu123@users.noreply.github.com>
2024-12-09 08:36:56 +0000

    connected

---
+++ basic.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+    {
+        int a = 2;
+        bool abc = 1 ;
+        cout<<"hello how are you "<<endl<< a << '\t' << abc <<endl ;
+    }
+
+
